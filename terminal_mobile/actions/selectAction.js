26931d481c6570655d53810e6bf2564a01b3035d
TMD <1@123.com>
2018-01-16 22:46:02 +0800

    客户端上传功能完成

--- terminal_mobile/actions/selectAction.js
+++ terminal_mobile/actions/selectAction.js
@@ -1,6 +1,6 @@
 export const selectImages = (images, current) =>({
   type:'SELECT_IMAGES',
-  imageUri:current.uri,
+  imageUri:images.length === 0 ? '' :current.uri,
   width:current.width,
   height:current.height,
 });
