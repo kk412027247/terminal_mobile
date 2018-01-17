7b725cce52caf03bc6751f8537bf378b59261910
TMD <1@123.com>
2018-01-17 22:42:10 +0800

    修改记录前端

--- terminal_mobile/person/person.js
+++ terminal_mobile/person/person.js
@@ -15,68 +15,79 @@ const Person = () =>(
     </Header>
     <Content padder>
       <Card style={styles.card}>
-        <CardItem header>
-          <Icon style={styles.saveIcon} name={'ios-cloud-done'}/>
-          <Text style={styles.saveTitle}>已保存</Text>
-        </CardItem>
-        <CardItem>
-          <Text style={styles.saveTitle}>厂商: </Text>
-          <Text>三星</Text>
-        </CardItem>
-        <CardItem>
-          <Text style={styles.saveTitle}>型号: </Text>
-          <Text>galaxy note 8</Text>
-        </CardItem>
-        <CardItem>
-          <Text style={styles.saveTitle}>TAC: </Text>
-          <Text>12345678</Text>
-        </CardItem>
-        <CardItem footer>
-          <Text>2018/01/15</Text>
-          <Right>
-            <Button
-              dark
-              style={styles.updateButton}
-              transparent
-              onPress={()=>{}}
-              title={''}
-            >
-              <Icon name={'md-create'}/>
-            </Button>
-          </Right>
-        </CardItem>
+        <View style={styles.left}>
+          <CardItem header>
+            <Icon style={styles.saveIcon} name={'ios-cloud-done'}/>
+            <Text style={styles.saveTitle}>已保存</Text>
+          </CardItem>
+          <CardItem>
+            <Text style={styles.saveTitle}>厂商: </Text>
+            <Text>三星</Text>
+          </CardItem>
+          <CardItem>
+            <Text style={styles.saveTitle}>型号: </Text>
+            <Text>galaxy note 8</Text>
+          </CardItem>
+          <CardItem>
+            <Text style={styles.saveTitle}>TAC: </Text>
+            <Text>12345678</Text>
+          </CardItem>
+          <CardItem footer>
+            <Text>2018/01/15</Text>
+            <Right>
+              <Button
+                dark
+                style={styles.updateButton}
+                transparent
+                onPress={()=>{}}
+                title={''}
+              >
+                <Icon name={'md-create'}/>
+              </Button>
+            </Right>
+          </CardItem>
+        </View>
+        <View style={styles.right}>
+          <Text>123</Text>
+        </View>
       </Card>
-      <Card>
-        <CardItem header>
-          <Icon style={styles.unSaveIcon} name={'ios-clock'}/>
-          <Text style={styles.unSaveTitle}>未保存</Text>
-        </CardItem>
-        <CardItem>
-          <Text style={styles.unSaveTitle}>厂商: </Text>
-          <Text>三星</Text>
-        </CardItem>
-        <CardItem>
-          <Text style={styles.unSaveTitle}>型号: </Text>
-          <Text>galaxy note 8</Text>
-        </CardItem>
-        <CardItem>
-          <Text style={styles.unSaveTitle}>TAC: </Text>
-          <Text>12345678</Text>
-        </CardItem>
-        <CardItem footer>
-          <Text>2018/01/15</Text>
-          <Right>
-            <Button
-              dark
-              style={styles.updateButton}
-              transparent
-              onPress={()=>{}}
-              title={''}
-            >
-              <Icon name={'md-create'}/>
-            </Button>
-          </Right>
-        </CardItem>
+
+      <Card style={styles.card}>
+        <View style={styles.left}>
+          <CardItem header>
+            <Icon style={styles.unSaveIcon} name={'ios-clock'}/>
+            <Text style={styles.unSaveTitle}>未保存</Text>
+          </CardItem>
+          <CardItem>
+            <Text style={styles.unSaveTitle}>厂商: </Text>
+            <Text>三星</Text>
+          </CardItem>
+          <CardItem>
+            <Text style={styles.unSaveTitle}>型号: </Text>
+            <Text>galaxy note 8</Text>
+          </CardItem>
+          <CardItem>
+            <Text style={styles.unSaveTitle}>TAC: </Text>
+            <Text>12345678</Text>
+          </CardItem>
+          <CardItem footer>
+            <Text>2018/01/15</Text>
+            <Right>
+              <Button
+                dark
+                style={styles.updateButton}
+                transparent
+                onPress={()=>{}}
+                title={''}
+              >
+                <Icon name={'md-create'}/>
+              </Button>
+            </Right>
+          </CardItem>
+        </View>
+         <View style={styles.right}>
+           <Text>456</Text>
+         </View>
       </Card>
       <View style={styles.pageButton}>
         <Button
@@ -101,6 +112,14 @@ const Person = () =>(
 const styles = StyleSheet.create({
   card:{
     marginBottom:10,
+    flexDirection:'row'
+  },
+  left:{
+    flex:1
+  },
+  right:{
+    width:100,
+    backgroundColor:'#c35ccc'
   },
   saveTitle:{
     fontWeight:'bold',
