20f9a70e2cbe0504231814d9f20e6e1d42c02ad2
TMD <1@123.com>
2018-02-26 20:31:55 +0800

    修复显示无限加载的bug

--- terminal_mobile/actions/historyAction.js
+++ terminal_mobile/actions/historyAction.js
@@ -1,8 +1,9 @@
 import host from '../../host';
-import realm from '../realm/schema';
+import realm, {UserInfoSchema} from '../realm/schema';
 import {handleNav} from '../actions/navAction';
 import {Toast} from 'native-base';
 import {Alert} from 'react-native';
+import Realm from "realm";
 
 const handleUserHistory = (history) =>({
   type:'USER_HISTORY',
@@ -40,13 +41,43 @@ export const getUserHistory = (loadMore=false)=>(
       }
       if(loadMore) await dispatch(handleSkip());
       const skip = getState().historyReducer.skip;
-      const res = await fetch(`http://${host}:3001/getUserHistory?skip=${skip}`);
-      const result = await res.json();
+      let res = await fetch(`http://${host}:3001/getUserHistory?skip=${skip}`);
+      let result = await res.json();
+
+      if(result[0] === 'needSession'){
+        const realm = await Realm.open({schema:[UserInfoSchema]});
+        const userInfo = realm.objects('userInfo').filtered('id=1');
+
+        if(!!realm.objects('userInfo').filtered('id=1')){
+          const _res = await fetch(`http://${host}:3001/signIn`,{
+            method:'post',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+              userName:userInfo[0].username,
+              passWord:userInfo[0].password,
+            })
+          });
+          const _result = await _res.json();
+          if(_result.level>=1 && _result.level <=5){
+            //如果登陆成功，递归执行一次本函数
+            res = await fetch(`http://${host}:3001/getUserHistory?skip=${skip}`);
+            result = await res.json();
+          }else{
+            dispatch(handleNav('SIGN_IN'));
+            return;
+          }
+        }else{
+          dispatch(handleNav('SIGN_IN'));
+          return;
+        }
+      }
       if(!!result.history){
         const history = skip === 0 ? result.history : [...getState().historyReducer.history, ...result.history] ;
         dispatch(toggleRefresh(false));
         dispatch(handleUserHistory(history));
-        if(result.history.length === 0){dispatch(stopLoadMore(true))}
+        if(result.history.length === 0){
+          dispatch(stopLoadMore(true))
+        }
       }
     })();
   }
